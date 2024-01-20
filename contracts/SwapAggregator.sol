8dd4b0300c68d54c789b8c92ef13866be074bd76
mohsinanees <mohsin.anees09@gmail.com>
2024-01-20 11:25:05 +0500

    execute payable for ETH -> ERC20 swap

--- contracts/SwapAggregator.sol
+++ contracts/SwapAggregator.sol
@@ -16,15 +16,25 @@ contract SwapAggregator {
      * @param calls An array of 'Call' structs containing target addresses and call data.
      * This function can be potentially risky as it can execute any arbitrary call data.
      */
-    function execute(Call[] memory calls) public {
+    function execute(Call[] memory calls) public payable {
         for (uint256 i = 0; i < calls.length; i++) {
             // Ensuring that the target address is not a zero address
             require(calls[i].target != address(0), "Invalid target address");
 
-            (bool success, ) = calls[i].target.call(calls[i].data);
-            require(success, "Call execution failed");
-
+            (bool success, bytes memory returnData) = calls[i].target.call{
+                value: msg.value / calls.length
+            }(calls[i].data);
+            if (!success) {
+                if (returnData.length < 68) revert("Call failed");
+                assembly {
+                    // Skip the first 0x20 bytes (32 bytes), then load the next 32 bytes
+                    returnData := add(returnData, 0x20)
+                }
+                revert(abi.decode(returnData, (string))); // Decode the revert reason and revert with it
+            }
             emit ExecutedCall(calls[i].target, calls[i].data);
         }
     }
+
+    function recieve() external payable {}
 }
--- migrations/deploy.js
+++ migrations/deploy.js
@@ -1,13 +1,51 @@
 const { ethers } = require('hardhat');
 
 async function main() {
-  // Get the contract to deploy
-  const swapAggregatorFactory = await ethers.getContractFactory('SwapAggregator');
-  const swapAggregator = await swapAggregatorFactory.deploy();
+  const localRpcUrl = 'http://localhost:8545'; // Change this to your local Hardhat RPC URL
+  const provider = new ethers.providers.JsonRpcProvider(localRpcUrl);
+
+  // Get the first signer using the custom provider
+  const [deployer] = await provider.listAccounts();
+  const signer = provider.getSigner(deployer);
+
+  // Log the current block number to confirm connection
+  console.log(await provider.getBlockNumber());
+
+  // Deploy the contract using the signer connected to the local RPC
+  const SwapAggregatorFactory = await ethers.getContractFactory('SwapAggregator', signer);
+  const swapAggregator = await SwapAggregatorFactory.deploy();
 
   await swapAggregator.deployed();
 
-  console.log('SwappingAggregator deployed to:', swapAggregator.address);
+  console.log('SwapAggregator deployed to:', swapAggregator.address);
+
+  // Impersonate the USDT holder
+  // const usdtHolderAddress = '0xee5B5B923fFcE93A870B3104b7CA09c3db80047A'; // Address of the USDT holder
+  // const myAddress = signer._address; // Your address
+  // console.log(myAddress);
+
+  // await network.provider.request({
+  //   method: 'hardhat_impersonateAccount',
+  //   params: [usdtHolderAddress],
+  // });
+
+  // const usdtHolder = await ethers.getSigner(usdtHolderAddress);
+
+  // // USDT Token Contract
+  // const usdtAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7'; // Mainnet USDT address
+  // const USDT = new ethers.Contract(
+  //   usdtAddress,
+  //   ['function transfer(address, uint256)', 'function balanceOf(address) view returns (uint256)'],
+  //   usdtHolder
+  // );
+
+  // // Transfer USDT
+  // const amount = ethers.utils.parseUnits('1', 6); // For example, 100 USDT
+  // await USDT.transfer(signer._address, amount);
+
+  // console.log('USDT Balance:', (await USDT.balanceOf(myAddress)).toString());
+
+  // console.log(`Transferred ${amount.toString()} USDT to ${myAddress}`);
 }
 
 main()
